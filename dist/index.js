ef6db69838545c8c4d6f2003d7ddc1c6cb763fa0
Ana Juárez <lisa13suarez@gmail.com>
2023-06-06 23:59:25 -0600

    add files

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log('Hola mundo');
+let mensaje = 'Hola Mundo';
+mensaje = 'Ja ja ja';
+mensaje = 'Bye Bye';
+console.log(mensaje);
+console.log(typeof []);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+console.log('Hola mundo')
+
+let mensaje: string = 'Hola Mundo'
+
+mensaje = 'Ja ja ja'
+mensaje = 'Bye Bye'
+console.log(mensaje)
+console.log(typeof [])
+
+
+let extensionDinosaurios: number = 76_000_000
+let dinosaurioFavorito: string = 'T_rex'
+let extinsion
+
+extinsion = 42
+
+extinsion = 'Hola'
+
+
+function saludar (config: any) {
+    return config
+}
+
+
+let animales: string[] = ['cerdo', 'gato', 'perro']
+let nums: number[] = [10, 5, 30]
+let checks: boolean[] = []
+
+let numeros: Array<number> = []
+
+
+//numeros.map(item => item.) //El autocompletado me sugiere el tipo de dato
