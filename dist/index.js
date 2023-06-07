f6ef3947f7b958dd59ae55c1f4ab2b8c43aed442
Ana Juárez <lisa13suarez@gmail.com>
2023-06-07 11:50:43 -0600

    add enums and type objects

--- dist/index.js
+++ dist/index.js
@@ -5,4 +5,31 @@ mensaje = 'Ja ja ja';
 mensaje = 'Bye Bye';
 console.log(mensaje);
 console.log(typeof []);
+let extensionDinosaurios = 76000000;
+let dinosaurioFavorito = 'T_rex';
+let extinsion;
+extinsion = 42;
+extinsion = 'Hola';
+function saludar(config) {
+    return config;
+}
+let animales = ['cerdo', 'gato', 'perro'];
+let nums = [10, 5, 30];
+let checks = [];
+let numeros = [];
+let tupla = [1, 'Hola', [10, 20, 50]];
+const peque = 's';
+const md = 'm';
+const grande = 'l';
+const extra = 'xl';
+var Talla;
+(function (Talla) {
+    Talla["Peque"] = "s";
+    Talla["Md"] = "m";
+    Talla["Grande"] = "l";
+    Talla["Extra"] = "xl";
+})(Talla || (Talla = {}));
+const variable1 = Talla.Grande;
+console.log(variable1);
+const estado = 2;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -30,3 +30,56 @@ let numeros: Array<number> = []
 
 
 //numeros.map(item => item.) //El autocompletado me sugiere el tipo de dato
+
+
+let tupla: [number, string, number[]] = [1, 'Hola', [10,20,50]]
+// tupla.push(12) //Este es un error pero TypeScript no lo reconoce
+
+
+const peque = 's'
+const md = 'm'
+const grande = 'l'
+const extra = 'xl'
+
+//Pascalcase
+enum Talla { Peque = 's', Md = 'm', Grande = 'l', Extra = 'xl' }
+
+const variable1 = Talla.Grande
+console.log(variable1)
+
+const enum LoadingState { Idle, Loading, Success, Error }
+
+const estado = LoadingState.Success
+
+//! Objetos
+type Direccion =  {
+    numero: number,
+    calle: string,
+    pais: string
+}
+
+type Persona = {
+    readonly id: number,
+    nombre: string,
+    talla: Talla,
+    direccion: Direccion
+}
+
+const objeto: Persona = {
+    id: 1,
+    nombre: '',
+    talla: Talla.Grande,
+    direccion: {
+        numero: 1,
+        calle: 'Nica',
+        pais: 'brazil'
+    }
+}
+
+const arr: Persona[] = []
+
+//objeto.nombre = 'Hola'
+
+
+
+
